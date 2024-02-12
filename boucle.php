70187cf131bdfad6cfead329f3ccb0cf0b6d2fed
GuillemotClement <guillemot.clement@protonmail.com>
2024-02-12 10:12:38 +0100

    Chapitre sur les boucles

--- boucle.php
+++ boucle.php
@@ -54,7 +54,7 @@ for($i = 0; $i < 10; $i += 2){
 // foreach($eleves as $classe => $eleve){
 //     echo "Dans la classe de $classe, il y a $eleve\n";
 // }
-
+/*
 $eleves = [
     "cm1" => ["Jean", "Henry", "Adeline", "Clément"],
     "cm2" => ["Marc", "Manu", "Vanessa", "Damien"],
@@ -65,3 +65,71 @@ foreach($eleves as $classe => $listEleve){
         echo "- $eleve\n";
     }
 }
+*/
+
+//on veut un système qui permet à l'utilisateur d'ajouter des notes, puis les afficher
+/*
+$notes = [];
+$action = null;
+while($action !== "fin"){
+    $action = readline("Saisir la note. Saisir fin lorsque cette tâche est terminée.\n");
+    if($action === 'fin'){
+        break; 
+    }
+    $notes[] =(int)$action;
+}
+foreach($notes as $note){
+    echo "- $note\n";
+}
+*/
+
+//on demande à l'utilisateur de saisir les horaire d'ouverture du magasin
+//on demande ensuite a l'user de rentrer une heure et on lui dit si le magasin est ouvert
+$horaire = [];
+
+while(true){
+    $debut = (int)readline("Saisir l'heure d'ouverture : ");
+    $fin = (int)readline("Saisir l'heure de fermeture : ");
+    //on vérifie que l'heure saisis est correct pour la plage
+    if($debut >= $fin){
+        echo "Erreur de saisis des horaires\n";
+        break;
+    }else{
+        //on vient créer un tableau de tableau qui contient la plage d'ouverture
+        $horaire[] = [$debut, $fin];
+        $action = readline("Souhaitez vous ajouter une plage d'ouverture ? (oui/non) : ");
+        //permet de sortir la boucle si l'user à terminer d'indiquer les plages d'ouverture
+        if($action !== "oui"){
+            break;
+        }
+    }
+}
+
+echo "Le magasin est ouvert de ";
+//par convention $k représente la clé lorsqu'il n'y a pas de valeur particulière
+foreach($horaire as $k => $creneau){
+    if($k > 0){
+        echo "et de ";
+    }
+    // permet d'interpoler correctement la variable { }
+    echo "{$creneau[0]}h à {$creneau[1]}h ";
+}
+
+/*
+$heure = readline(("Saisir une heure d'arrivée :"));
+//$creneauTrouver permet de savoir si l'heure est trouver dans une des plage d'ouverture
+$creneauTrouver = false;
+foreach($horaire as $creneau){
+    //si l'heure saisis est trouver dans la plage, alors on passe la valeur de $creneauTrouver à true
+    if($heure >= $creneau[0] && $heure <= $creneau[1]){
+        $creneauTrouver = true;
+        break;
+    }
+}
+//si l'heure est dans un crenau, alors on affiche le message
+if($creneauTrouver){
+    echo "Le magasin sera ouvert.";
+}else{
+    echo "Le magasin sera fermé.";
+}
+*/
