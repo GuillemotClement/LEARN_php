5e364b04c72f9ddd6ed980c0862d23c7130e5fbd
GuillemotClement <guillemot.clement@protonmail.com>
2024-02-14 20:31:05 +0100

    Chapitre HTML et CSS

---
+++ contact.php
@@ -0,0 +1,6 @@
+<?php include 'header.php' ?>
+
+<main class="container">
+    <h1 class="text-uppercase text-center">contact</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores harum ut optio rerum, molestiae quia natus dolor nihil sit quisquam suscipit cum assumenda? Vero iste fugiat assumenda unde laboriosam maiores.</p>
+</main>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//on vient importer le code du header qui est commune à chaque page du site.
+require 'header.php';
+?>
+
+
+
+<?php 
+//on vient imporer le footer qui est commun à chaque page
+require 'footer.php';
+
+//démarrer un serveur local php
+// $ php -S localhost:8000 dans le terminal
