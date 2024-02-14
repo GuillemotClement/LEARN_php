1051e18411abeb5ea63698552981b7507f65107a
GuillemotClement <guillemot.clement@protonmail.com>
2024-02-14 17:25:51 +0100

    Ajout chapitre functionUser

--- learn/functionUser.php
+++ learn/functionUser.php
@@ -33,5 +33,38 @@ function repondreOuiNon($sentence){
         }
     }
 }
-$answer = repondreOuiNon("Souhaitez vous continuer (o/n) :");
-var_dump($answer);
+// $answer = repondreOuiNon("Souhaitez vous continuer (o/n) :");
+// var_dump($answer);
+
+function demander_creneau(string $sentence = "Veuillez entre un crenau"){
+    echo $sentence . "\n";
+    while(true){
+        $ouverture = (int)readline("Heure d'ouverture : ");
+        if($ouverture >= 0 && $ouverture <= 23){
+            break;
+        }
+    }
+    while(true){
+        $fermeture = (int)readline("Heure de fermeture : ");
+        if($fermeture >= 0 && $fermeture <= 23 && $fermeture > $ouverture){
+            break;
+        }
+    }
+    return [$ouverture, $fermeture];
+} 
+
+//le typage permet de vérouiller le type de données que peux prendre une fonction
+//le point d'interrogation indique que le paramètre peut être null
+function demander_creneaux(?string $sentence = "Saisir vos créneaux : ") :array
+{
+    $creneaux = [];
+    $continuer = true;
+    while($continuer){
+        $creneaux[] = demander_creneau();
+        $continuer = repondreOuiNon("voulez vous continuer ? ");
+    }
+    return $creneaux;
+}
+
+$creneaux = demander_creneaux();
+print_r($creneaux);
