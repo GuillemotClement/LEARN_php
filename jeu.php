e1180ae2747ce74c126c416c641fc83f9d04e73e
GuillemotClement <guillemot.clement@protonmail.com>
2024-02-17 10:14:20 +0100

    🎨 Amélioration du formulaire pour deviner le nombre

--- jeu.php
+++ jeu.php
@@ -1,24 +1,32 @@
 <?php 
 $aDeviner = 150;
+$erreur = null;
+$succes = null;
+$value = null;
+
+if(isset($_GET['chiffre'])){
+    if($_GET['chiffre'] > $aDeviner){
+        $erreur = "Le chiffre est trop grand";
+    }elseif($_GET['chiffre'] < $aDeviner){
+        $erreur = "Le chiffre est trop petit";
+    }else{
+        $succes = "Bravo, vous avez deviner";
+    }
+    $value = (int)$_GET['chiffre'];
+}
 
 require 'header.php'; 
-
-
-function setValue($string){
-    if(isset($string)){
-        return htmlentities($string);
-    }
-}
 ?>
 
-<?php if(isset($_GET['chiffre'])) :?>
-    <?php if($_GET['chiffre'] > $aDeviner): ?>
-        Le chiffre est trop grand
-    <?php elseif($_GET['chiffre'] < $aDeviner): ?>
-        Le chiffre est trop petit
-    <?php else:?>
-        Bravo, vous avez deviner 
-    <?php endif?>
+
+<?php if($erreur):?>
+    <div class="alert alert-danger">
+        <?= $erreur ?>
+    </div>
+<?php elseif($succes):?>
+    <div class="alert alert-success">
+        <?=$succes?>
+    </div>
 <?php endif?>
 
 
@@ -27,14 +35,13 @@ function setValue($string){
         <h3>Mon petit formulaire</h3>
         <div class="mb-3">
             <label for="" class="form-label"></label>
-            <input type="number" class="form-control" id="" name="chiffre" placeholder="entre 0 et 1000" value="<?=setValue($_GET['chiffre'])?>">
+            <input type="number" class="form-control" id="" name="chiffre" placeholder="entre 0 et 1000" value="<?=$value?>">
         </div>
         <div class="ctn-btn">
             <button type="submit" class="btn btn-primary shadow">Deviner</button>
         </div>
     </form>
 </main>
-
 
 
 <?php 
