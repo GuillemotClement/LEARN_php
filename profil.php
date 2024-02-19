d456d37d95c929fa8ccb3c9916159e9333b7cc82
GuillemotClement <guillemot.clement@protonmail.com>
2024-02-19 10:13:11 +0100

    ✨ reset du cookie

--- profil.php
+++ profil.php
@@ -1,5 +1,12 @@
 <?php 
 $username = null;
+//creation de la clear cookie
+if(!empty($_GET['action']) && $_GET['action'] === 'deconnecter'){
+    //on détruit la valeur de 'utilisateur' dans $_cookie
+    unset($_COOKIE['utilisateur']);
+    //on fait un retour dans le passé sur le cookie pour le reset
+    setcookie('utilisateur', '', time()-10);
+}
 //on vérifie que le cookie est définit et qu'il contient bien la valeur username
 if(!empty($_COOKIE['utilisateur'])){
     $username = $_COOKIE['utilisateur'];
@@ -8,7 +15,7 @@ if(!empty($_COOKIE['utilisateur'])){
 //on vérifie post a une valeur et on créer un cookie avec le nom que l'user à indiquer dans le formulaire
 //ce cookie est valable le temps que l'user ne quitte pas le navigateur
 if(!empty($_POST['username'])){
-    setcookie('username', $_POST['username']);
+    setcookie('username', $_POST['username'], time()+60*60*24*30);
     $username = $_POST['username'];
 }
 
@@ -21,6 +28,8 @@ require 'elements/header.php';
         <div class="alert alert-success">
             Bonjour <?= htmlentities($username)?>
         </div>
+        <!-- Creation d'un lien qui permet de clear le cookie -->
+        <a href="profil.php?action=deconnecter">Logout</a>
     <?php else:?>
         <form action="" method="post">
             <label for="username">Name :</label>
